0aff188f5397401b7b583c0ec8aa17a5ecf5089d
rammahidhar <66819227+rammahidhar@users.noreply.github.com>
2023-07-21 14:44:30 +0530

    Add files via upload

---
+++ python program to print all positive numbers in a range {task 2}.py
@@ -0,0 +1,15 @@
+# pyhton program to print all the positive numbers in a range.
+
+list1 = [12,-7,5,64,-14]
+for i in list1:
+    if i<0:
+        list1.remove(i)
+print(list1)
+
+
+
+list2 = [12,14,-95,3]
+for i in list2:
+    if i<0:
+        list2.remove(i)
+print(list2)
